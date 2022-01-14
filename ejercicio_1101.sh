c143a8dcf98ff52b051968ed0d70b2d420d4f8a3
JessicaVerdezoto <95991913+JessicaVerdezoto@users.noreply.github.com>
2022-01-13 23:28:46 -0500

    Create ejercicio_1101.sh

---
+++ ejercicio_1101.sh
@@ -0,0 +1,6 @@
+# Ejercicio 1.10.1 
+# Se trabaja con datos de secuenciacion de proxima generacion (NGS).
+# # A continuacion se subio el archivo Marra2014_data.fasta
+# # Nos ubicamos en el directorio donde se va a trabajar
+cd Desktop/Sexto\ Sem/CSB/unix/data/
+#  Cambie de directorio
