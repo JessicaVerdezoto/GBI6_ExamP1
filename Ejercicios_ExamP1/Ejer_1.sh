732f3317cd12ca8bc0a9342acbff77f68b42f513
JessicaVerdezoto <95991913+JessicaVerdezoto@users.noreply.github.com>
2022-01-15 11:12:14 -0500

    Update Ejer_1.sh

--- Ejercicios_ExamP1/Ejer_1.sh
+++ Ejercicios_ExamP1/Ejer_1.sh
@@ -3,4 +3,11 @@ cd Desktop/Sexto Sem/Bioinfo/Exa_P1
 mkdir test
 touch test1.txt
 touch test1/test1.txt
-touch 
+touch test1/test2.txt
+echo "Hola GBI6" >> test1/test2.txt
+touch test1/test1.py
+echo "print('Hola GBI6')" >> test1/test1.py
+cat test1/test2.txt
+Hola GBI6
+wc -c test1/test2.txt
+10 test1/test2.txt
