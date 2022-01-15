55cece085fdd8a4f9493b1d0948e0a89a0a17ed7
JessicaVerdezoto <95991913+JessicaVerdezoto@users.noreply.github.com>
2022-01-15 11:28:42 -0500

    Create Ejer_3.sh

---
+++ Ejercicios_ExamP1/Ejer_3.sh
@@ -0,0 +1,16 @@
+#Para esta pregunta debe realizar previamente lo siguiente:
+cd Desktop/Sexto Sem/Bioinfo/Exa_P1
+pwd
+/c/Users/INTEL/Desktop/Sexto Sem/Bioinfo/Exa_P1
+mkdir test
+touch test1.txt
+touch test1/test1.txt
+touch test1/test2.txt
+echo "Hola GBI6" >> test1/test2.txt
+touch test1/test1.py
+echo "print('Hola GBI6')" >> test1/test1.py
+mkdir test2
+mkdir test2/subtest1
+cp test1/test2.txt test2/subtest1/test2.txt
+rm -r test1
+
