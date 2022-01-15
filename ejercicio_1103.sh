2818f7875d9c0f1621c6141689aa7ca4fd434dfe
JessicaVerdezoto <95991913+JessicaVerdezoto@users.noreply.github.com>
2022-01-14 21:41:38 -0500

    Update ejercicio_1103.sh

--- ejercicio_1103.sh
+++ ejercicio_1103.sh
@@ -1,7 +1,6 @@
 # Ejercicio 1.10.3 
 # Escribe un guión que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están separadas por espacios y que hay un espacio al final de cada línea. Su guión debería devolver.
 # # Primero se creo el reposito con nombre GBI6_EXAMP1
-# # A continuacion se subio la carpeta de Saavedra2013
 # # Nos ubicamos en el directorio donde se va a trabajar
 cd Saavedra2013
 #  Pregunta 1
